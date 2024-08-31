4b511198b5fc6bd76a01e9a7034706765b32a285
Chrisser1 <chrisgthomsen0310@gmail.com>
2024-08-31 21:57:37 +0200

    Init

---
+++ Misc/PracticeExercises/test/src/App.java
@@ -0,0 +1,11 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        hello();
+    }
+
+    private static void hello() {
+        System.out.println("Hello, World");
+    }
+}
