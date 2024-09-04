999bfe0903dab18973befe2e90370b5e1a4cddab
Chrisser <chrisgthomsen0310@gmail.com>
2024-09-04 10:28:20 +0200

    added weeks

---
+++ Assignments/uge 1/Opgave 4/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public static void printTrianglesSameLine() {
+        System.out.println("/\\          /\\");
+        System.out.println("/  \\        /  \\");
+        System.out.println("/    \\      /    \\");
+    }
+
+    public static void printCubesSameLine() {
+        System.out.println("+------+    +------+");
+        System.out.println("/    \\      /    \\");
+        System.out.println("/    \\      /    \\");
+        System.out.println("/    \\      /    \\");
+        System.out.println("/    \\      /    \\");
+    }
+
+    public static void
+}
