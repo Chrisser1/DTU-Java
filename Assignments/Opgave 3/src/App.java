7c80d4c61508970ec40342c52463d007de552276
Chrisser <chrisgthomsen0310@gmail.com>
2024-09-04 10:25:52 +0200

    made assignments 1 - 3

---
+++ Assignments/Opgave 3/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("//////////////////////");
+        for (int i = 0; i < 5; i++) {
+            printVictory();
+        }
+    }
+
+    public static void printVictory() {
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
--- Misc/PracticeExercises/test/src/App.java
+++ Misc/PracticeExercises/test/src/App.java
@@ -1,9 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class App {
-    public static void main(String[] args) throws Exception {
-        hello();
-    }
+    static Author h = new Author("Christoffer", 20);
+    public static void main(String[] args) throws IOException {
+        System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "Input your name" + ConsoleColors.RESET);
 
-    private static void hello() {
-        System.out.println("Hello, World");
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        String name = reader.readLine();
+
+        System.out.println(ConsoleColors.GREEN_BOLD + "Your name is: "+ name);
+        System.out.println(ConsoleColors.RED_BOLD_BRIGHT + h);
+        System.out.print(ConsoleColors.RESET);
     }
 }
