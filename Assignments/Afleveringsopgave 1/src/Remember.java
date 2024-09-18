5655a18f9d367c13e1328097205238625b4f3506
N1njaflam1ng0 <82931846+N1njaflam1ng0@users.noreply.github.com>
2024-09-18 15:53:26 +0200

    err to out
    ændrede err to out ved nogle println's

--- Assignments/Afleveringsopgave 1/src/Remember.java
+++ Assignments/Afleveringsopgave 1/src/Remember.java
@@ -1,8 +1,8 @@
 public class Remember {
     public static void main(String[] args) throws Exception {
         String s = "Use \"\\\\\" to obtain a  ’backslash’ character";
-        System.err.println(s);
+        System.out.println(s);
         System.out.println("Remember:");
-        System.err.println(s);
+        System.out.println(s);
     }
 }
