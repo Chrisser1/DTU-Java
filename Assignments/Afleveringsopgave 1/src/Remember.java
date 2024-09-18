19d6c42880bdaa5db2183fe48a9962ee46b83c09
N1njaflam1ng0 <82931846+N1njaflam1ng0@users.noreply.github.com>
2024-09-18 16:21:46 +0200

    Kommentar til kode

--- Assignments/Afleveringsopgave 1/src/Remember.java
+++ Assignments/Afleveringsopgave 1/src/Remember.java
@@ -1,5 +1,6 @@
 public class Remember {
     public static void main(String[] args) throws Exception {
+        //Setting the String s to the first line of the console prints
         String s = "Use \"\\\\\" to obtain a  ’backslash’ character";
         System.out.println(s);
         System.out.println("Remember:");
--- Assignments/Afleveringsopgave 1/src/UsingClassConstant.java
+++ Assignments/Afleveringsopgave 1/src/UsingClassConstant.java
@@ -1,6 +1,8 @@
 public class UsingClassConstant {
+    //Creating a Class CONSTANT
     public static final int LINES = 3;
 
+    //Function to print using the Class CONSTANT
     public static void main(String[] args) {
         for (int i = 1; i <= LINES; i++) {
             for (int j = 1; j <= LINES; j++) {
