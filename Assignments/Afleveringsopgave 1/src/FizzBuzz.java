7617405feed085306185323b34c70a052f027029
Chrisser <chrisgthomsen0310@gmail.com>
2024-09-18 16:28:30 +0200

    Added comments to FizzBuzz.java

--- Assignments/Afleveringsopgave 1/src/FizzBuzz.java
+++ Assignments/Afleveringsopgave 1/src/FizzBuzz.java
@@ -1,11 +1,14 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 30; i++) {
-            boolean a = i % 3 == 0;
-            boolean b = i % 5 == 0;
-            
-            System.out.print(a ? "Fizz" : "");
-            System.out.print(b ? "Buzz" : "");
+            boolean a = i % 3 == 0; // check if the number is divisible by 3
+            boolean b = i % 5 == 0; // check if the number is divisible by 5
+
+            System.out.print(a ? "Fizz" : ""); // if a is true then print Fizz else nothing
+            System.out.print(b ? "Buzz" : ""); // if b is true then print Buzz else nothing
+            /* if a and b is false then print
+               the current number on a new line
+               else print nothing and go to a new line */
             System.out.println(!a && !b ? i : "");
         }
     }
