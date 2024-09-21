9f96e3acb4ff20d93efc7caa0869e03149daadb0
Zliptember <70944769+Zliptember@users.noreply.github.com>
2024-09-22 00:55:35 +0200

    Ændrede variabelnavne til at være mere relevante

--- Assignments/Afleveringsopgave 1/src/FizzBuzz.java
+++ Assignments/Afleveringsopgave 1/src/FizzBuzz.java
@@ -2,12 +2,12 @@ public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 30; i++) {
             // checking divisibility of number i and assigning truth value to boolean
-            boolean a = i % 3 == 0;
-            boolean b = i % 5 == 0; 
+            boolean fizz = i % 3 == 0;
+            boolean buzz = i % 5 == 0; 
 
-            System.out.print(a ? "Fizz" : ""); // if a is true then print Fizz else nothing
-            System.out.print(b ? "Buzz" : ""); // if b is true then print Buzz else nothing
-            System.out.println(!a && !b ? i : ""); // if not a and not b, print number, else nothing and new line
+            System.out.print(fizz ? "Fizz" : ""); // if fizz is true then print Fizz else nothing
+            System.out.print(buzz ? "Buzz" : ""); // if buzz is true then print Buzz else nothing
+            System.out.println(!fizz && !buzz ? i : ""); // if neither fizz or buzz, print number, else nothing and new line
         }
     }
 }
