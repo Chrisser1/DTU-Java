f3b5fec4ab05e608678a62deeeeca59ab37dba8c
Chrisser1 <97701175+Chrisser1@users.noreply.github.com>
2024-09-22 14:16:46 +0200

    Update FizzBuzz.java
    Removed comments

--- Assignments/Afleveringsopgave 1/src/FizzBuzz.java
+++ Assignments/Afleveringsopgave 1/src/FizzBuzz.java
@@ -1,13 +1,13 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 30; i++) {
-            // checking divisibility of number i and assigning truth value to boolean
+            // Checking divisibility of number i and assigning truth value to boolean
             boolean fizz = i % 3 == 0;
             boolean buzz = i % 5 == 0; 
 
-            System.out.print(fizz ? "Fizz" : ""); // if fizz is true then print Fizz else nothing
-            System.out.print(buzz ? "Buzz" : ""); // if buzz is true then print Buzz else nothing
-            System.out.println(!fizz && !buzz ? i : ""); // if neither fizz or buzz, print number, else nothing and new line
+            System.out.print(fizz ? "Fizz" : "");
+            System.out.print(buzz ? "Buzz" : "");
+            System.out.println(!fizz && !buzz ? i : ""); // If neither fizz or buzz, print number, else nothing and new line
         }
     }
 }
