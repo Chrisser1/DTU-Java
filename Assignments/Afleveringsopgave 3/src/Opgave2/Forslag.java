456dc07896b85052c47cb547b5de1ebc90585803
N1njaflam1ng0 <82931846+N1njaflam1ng0@users.noreply.github.com>
2024-10-28 11:25:33 +0100

    New Branch

---
+++ Assignments/Afleveringsopgave 3/src/Opgave2/Forslag.java
@@ -0,0 +1,11 @@
+package Opgave2;
+
+public class Forslag {
+    //We create the 2 two fields of the class
+    private String navn, sted;
+
+    public void Forslag (String navn, String sted) {
+        this.navn = navn;
+        this.sted = sted;
+    }
+}
